84bc7079ad67552a8abe74938d36f16ed35855fa
Raymond Bertram <raymond.bertram@gmx.de>
2025-04-13 21:36:42 +0200

    24-minor-enhancements

--- src/screens/footer/footer.screen.jsx
+++ src/screens/footer/footer.screen.jsx
@@ -5,7 +5,13 @@ export const Footer = () => {
   return (
     <div className="w-full px-15 pt-15 pb-5 rounded-[18px] md:h-fit flex flex-col-reverse md:flex-row gap-4 items-center md:justify-between md:p-8">
       <div className="flex flex-col md:flex-row md:gap-4 text-center items-center">
-        <img src={logo} alt="logo" width={60} height={60} />
+        <img
+          className="mb-4 md:m-0"
+          src={logo}
+          alt="logo"
+          width={60}
+          height={60}
+        />
         <p>Copyright © 2025 HELLO TRAFFIC. All rights reserved.</p>
       </div>
       <ul className="list-none p-0 m-0 flex flex-col md:flex-row gap-4 text-center">
